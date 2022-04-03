a39db4da5ab02ba911f3912ac2496156adc6601b
SiriusKoan <k.peihsun@gmail.com>
2022-04-03 15:10:06 +0800

    Add netcat

--- monitor/utils.py
+++ monitor/utils.py
@@ -1,4 +1,5 @@
 import abc
+import socket
 import os
 import subprocess
 import smtplib
@@ -193,6 +194,35 @@ class NotifyEmail(MonitoringFunc):
                 self.compose_msg("recover", self.recover_msg),
             )
 
+class Netcat:
+    """ Python 'netcat like' module """
+    """ The snippet is from https://gist.github.com/leonjza/f35a7252babdf77c8421 """
+
+    def __init__(self, ip, port, timeout):
+        self.buff = ""
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(timeout)
+        self.ip = ip
+        self.port = port
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.socket.close()
+
+    def connect(self):
+        try:
+            self.socket.connect((self.ip, self.port))
+            return True
+        except socket.timeout:
+            return False
+
+    def read(self, length=1024):
+        return self.socket.recv(length)
+
+    def write(self, data):
+        self.socket.send(data)
 
 def ping(host, args="-c 1"):
     res = subprocess.call(["ping", *args.split(), host.ip_addr], stdout=open(os.devnull, "w"), stderr=open(os.devnull, "w"))
