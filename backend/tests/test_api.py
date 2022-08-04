e4a0ba5f5e0e3b75db72b2fbe07b63e5b04ab185
SiriusKoan <k.peihsun@gmail.com>
2022-08-04 15:04:32 +0800

    feat: use pytest

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,37 +1,25 @@
-import unittest
+import pytest
 from flask import url_for
 from app import create_app
 
 
-class APIHostsTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.app = create_app("testing")
-        self.client = self.app.test_client()
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+@pytest.fixture
+def client():
+    app = create_app("testing")
+    client = app.test_client()
+    app_context = app.app_context()
+    app_context.push()
+    yield client
+    if app_context:
+        app_context.pop()
 
-    def tearDown(self) -> None:
-        if self.app_context:
-            self.app_context.pop()
-
-    def test_alive(self):
-        response = self.client.get(url_for("main.get_hosts_api"))
-        self.assertEqual(response.status_code, 200)
+def test_api_hosts(client):
+    response = client.get(url_for("main.get_hosts_api"))
+    assert response.status_code == 200
 
 
-class APIFuncResultsTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.app = create_app("testing")
-        self.client = self.app.test_client()
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-
-    def tearDown(self) -> None:
-        if self.app_context:
-            self.app_context.pop()
-
-    def test_alive(self):
-        response = self.client.get(
-            url_for("main.get_results_api", host="test", func="test")
-        )
-        self.assertEqual(response.status_code, 200)
+def test_api_func_results(client):
+    response = client.get(
+        url_for("main.get_results_api", host="test", func="test")
+    )
+    assert response.status_code == 200
