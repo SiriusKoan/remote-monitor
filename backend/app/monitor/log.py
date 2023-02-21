acad72df457078b92dd7620caefe14164bb0a714
SiriusKoan <me@siriuskoan.one>
2023-02-21 03:17:44 -0500

    feat: add file log

--- backend/app/monitor/log.py
+++ backend/app/monitor/log.py
@@ -2,7 +2,7 @@ import logging
 import logging.handlers
 from . import config
 
-class StreamFormatter(logging.Formatter):
+class Formatter(logging.Formatter):
     def __init__(self, fmt):
         self.color_table = {
             logging.CRITICAL: "\033[31m",
@@ -20,8 +20,18 @@ class StreamFormatter(logging.Formatter):
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+# console handler
 stream_handler = logging.StreamHandler()
-stream_format = StreamFormatter(config.CONSOLE_LOG_FORMAT)
+stream_format = Formatter(config.CONSOLE_LOG_FORMAT)
 stream_handler.setLevel(config.SENDING_LEVEL.get("console"))
 stream_handler.setFormatter(stream_format)
 logger.addHandler(stream_handler)
+
+# file handler
+file_handler = logging.FileHandler(config.LOG_FILE)
+file_format = Formatter(config.FILE_LOG_FORMAT)
+file_handler.setLevel(config.SENDING_LEVEL.get("file"))
+file_handler.setFormatter(file_format)
+logger.addHandler(file_handler)
+
